58fe9e12376ce7183e7e398fbb65c6041d22efbe
amandayu1 <amanda_yu2002@live.com>
2021-01-05 20:04:03 -0500

    added working template

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, TextInput, View, Button} from 'react-native';
 
 export default function App() {
+  const [outputText, setOutputText] = useState("Open up App.js to start working on your app!")
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={{padding: 30}}>
+      <TextInput placeholder="What's up? Type here." style={{padding: 10, borderColor: "blue", borderWidth:1}}/>
+      <Button title="Hello, bitches"/>
+      
+      </View>
+      
+      <View>
+      <Text></Text>
+      </View>
+      
+    </View> 
   );
 }
 
@@ -16,6 +25,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center', },
+   
 });
