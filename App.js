ad90ce94c21164284fb9c638bacee4f62d56129a
Amanda Yu20 <amanda_yu2002@live.com>
2021-01-07 19:34:38 -0500

    added tabs and navigation from sign in to tabs

--- App.js
+++ App.js
@@ -6,30 +6,49 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 
 import { SignIn, CreateAccount, Profile, Home} from "./src/screens/SignIn";
 
-export default function App() {
-  const AuthStack = createStackNavigator();
-  const Tabs = createBottomTabNavigator();
+const AuthStack = createStackNavigator();
+const Tabs = createBottomTabNavigator();
+const HomeStack = createStackNavigator();
+const ProfileStack = createStackNavigator();
 
+const HomeStackScreen = () => (
+  <HomeStack.Navigator>
+    <HomeStack.Screen name="Home" component={Home}/>
+  </HomeStack.Navigator>
+)
+
+const ProfileStackScreen = () => (
+  <ProfileStack.Navigator>
+    <ProfileStack.Screen name="Profile" component={Profile}/>
+  </ProfileStack.Navigator>
+)
+
+export default function App() {
+  const [userToken, setUserToken] = React.useState(null);
+
+  
   return (
-    <NavigationContainer>
-      <Tabs.Navigator>
-        <Tabs.Screen name="Home" component={Home}/>
-        <Tabs.Screen name="Profile" component={Profile}/>
-      </Tabs.Navigator>
-      {/*
+    <NavigationContainer style={styles.container}>
+      {userToken ? (
+        <Tabs.Navigator style={styles.container}>
+        <Tabs.Screen name="Home" component={HomeStackScreen}/>
+        <Tabs.Screen name="Profile" component={ProfileStackScreen}/>
+        </Tabs.Navigator>
+      ):
+      
       <AuthStack.Navigator initialRouteName="SignIn">
-        <AuthStack.Screen
-          name="SignIn"
-          component={SignIn}
-          options={{ title: "Sign In" }}
-        />
-        <AuthStack.Screen
-          name="CreateAccount"
-          component={CreateAccount}
-          options={{ title: "Create Account" }}
-        />
+      <AuthStack.Screen
+        name="SignIn"
+        component={SignIn}
+        options={{ title: "Sign In" }}
+      />
+      <AuthStack.Screen
+        name="CreateAccount"
+        component={CreateAccount}
+        options={{ title: "Create Account" }}
+      />
       </AuthStack.Navigator>
-      */}
+      }
   </NavigationContainer> 
   );
 }
--- src/screens/CreateAccount.js
+++
@@ -1,7 +0,0 @@
-import React, { useState } from 'react';
-import { ScreenContainer } from "react-native-screens";
-import { StyleSheet, Text, View, Button} from 'react-native';
-
-
-
-export default CreateAccount;
